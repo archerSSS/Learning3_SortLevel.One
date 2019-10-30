7bc0cf8848c2344e70fd69a7ee18531c711a59a1
archerSSS <archyrysss@gmail.com>
2019-10-30 22:20:55 +0300

    Algo_3: SortLevelLesson_v.0.1

---
+++ SortSpace/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortSpace
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] { 4, 2, 1, 5, 3, 11 };
+            SortLevel.SelectionSortStep(arr, 0);
+            SortLevel.BubbleSortStep(arr);
+        }
+    }
+}
---
+++ SortSpace/SortLevel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortSpace
+{
+    public static class SortLevel
+    {
+        public static void SelectionSortStep(int[] array, int i)
+        {
+            if (i < array.Length - 1) ExchangeValues(array, i, MinorElement(array, i + 1));
+        }
+
+        public static bool BubbleSortStep(int[] array)
+        {
+            bool Compared = false;
+            bool Changed = false;
+            for (int i = 0; ; i++)
+            {
+                if (i + 1 == array.Length)
+                {
+                    if (Changed)
+                    {
+                        if (!Compared) return false;
+                        i = 0;
+                        Compared = false;
+                    }
+                    else return true;
+                }
+                if (Compare(array[i], array[i + 1]))
+                {
+                    Changed = true;
+                    Compared = Changed;
+                    ExchangeValues(array, i, i + 1);
+                }
+            }
+        }
+
+        public static bool Compare(int a, int b)
+        {
+            return a > b;
+        }
+
+        public static int MinorElement(int[] array, int i)
+        {
+            int minor = i;
+            for (; i < array.Length; i++)
+                if (array[i] < array[minor]) minor = i;
+            return minor;
+        }
+
+        public static void ExchangeValues(int[] array, int i, int j)
+        {
+            array[i] += array[j];
+            array[j] = array[i] - array[j];
+            array[i] = array[i] - array[j];
+        }
+    }
+}
