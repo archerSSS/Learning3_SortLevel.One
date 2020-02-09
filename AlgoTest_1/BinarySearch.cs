a1e8793b2dc8ec711748beef3937e9e029f8329c
archerSSS <archyrysss@gmail.com>
2020-02-10 00:40:06 +0300

    Algo_3: BinarySearchLesson_v.0.1

---
+++ AlgoTest_1/BinarySearch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest_1
+{
+    public class BinarySearch
+    {
+        public int Left;
+        public int Right;
+        private int result;
+        private int[] array;
+        
+
+        public BinarySearch(int[] array)
+        {
+            Left = 0;
+            Right = array.Length - 1;
+            result = 0;
+            this.array = array;
+        }
+
+        public void Step(int N)
+        {
+            int middle = (Right - Left) / 2;
+            
+            if (array[middle] > N)
+                Right = (Right - 1) / 2;
+            else if (array[middle] < N)
+                Left = (Right + 1) / 2;
+            else if (array[middle] == N) result = 1;
+            else result = -1;
+        }
+
+        public int GetResult()
+        {
+            return result;
+        }
+    }
+}
---
+++ AlgoTest_1/UnitTest4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortSpace;
+
+namespace AlgoTest_1
+{
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] array = new int[] { 1, 3, 4, 7, 9, 10 };
+
+            BinarySearch BS = new BinarySearch(array);
+            Assert.AreEqual(0, BS.GetResult());
+
+            BS.Step(3);
+            Assert.AreEqual(0, BS.Left);
+            Assert.AreEqual(2, BS.Right);
+            Assert.AreEqual(0, BS.GetResult());
+
+            BS.Step(3);
+            Assert.AreEqual(0, BS.Left);
+            Assert.AreEqual(2, BS.Right);
+            Assert.AreEqual(1, BS.GetResult());
+        }
+    }
+}
